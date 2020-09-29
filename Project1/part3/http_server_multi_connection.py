810084fa20cef1e14a8c3d232673ae3abfaed13b
tbayvas <58962386+tbayvas@users.noreply.github.com>
2020-09-29 15:18:55 -0500

    http server multi connection additions

--- Project1/part3/http_server_multi_connection.py
+++ Project1/part3/http_server_multi_connection.py
@@ -1,7 +1,22 @@
 import socket
+import select
 
 class HttpServerMultiConnection:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     def open_connection(self, port: int):
         print(port)
+        self.sock.bind(('', port))
+
+        self.sock.listen(2)
+        #open_conns = []
+        read_list = []
+        conn, addr = self.sock.accept()
+        read_list.append(conn)
+        outputs = []
+
+        while read_list:
+            select.select(read_list, outputs)
+
+        while True:
+            data = conn.recv(4096)
