9f0628b25bd6c2cdc3f8af1a6a16b6bb4d0a1496
Adnan Can TURKAY <acturkay@gmail.com>
2020-09-29 21:37:20 +0300

    --

--- Project1/packages/request_builder.py
+++ Project1/packages/request_builder.py
@@ -4,15 +4,29 @@ from packages.http_params import HttpMessageHeader, get_header
 class RequestBuilder:
     messages = {}
 
+    def receive_data(self, addr: str, data: str) -> str:
+        if addr not in self.messages:
+            self.messages[addr] = UnFinishedRequest()
+
+        self.messages[addr].receive_data(data)
+        if self.messages[addr].body_bytes is not None and\
+                self.messages[addr].body_bytes == self.messages[addr].content_length:
+            return self.messages[addr].message
+
+        return None
+
 
 class UnFinishedRequest:
     message = None
     body_bytes = None
     content_length = None
 
-    def receive_data(self, data):
-        self.message += data
-        lines = self.message.split(b'\r\n')
+    def receive_data(self, data: str):
+        if self.message is None:
+            self.message = data
+        else:
+            self.message += data
+        lines = self.message.split('\r\n')
         if self.content_length is None:
             for line in lines:
                 header = get_header(line)
@@ -22,8 +36,7 @@ class UnFinishedRequest:
                     except:
                         print("Could not parse content length from header:\n" + header.key + ":" + header.value)
 
-
-
-    def __init__(self, message, body_bytes):
-        self.message = message
-        self.body_bytes = body_bytes
+        if self.content_length is not None:
+            parts = self.message.split('\r\n\r\n')
+            if len(parts) > 1:
+                self.body_bytes = len(parts[1])
---
+++ Project1/tests/intermittent_data_tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+from packages.request_builder import RequestBuilder
+
+
+class IntermittentDataTests(unittest.TestCase):
+    def test1(self):
+        rb = RequestBuilder()
+        assert rb.receive_data('1', 'GET') is None
+        assert rb.messages['1'].message == 'GET'
+
+        assert rb.receive_data('1', ' /basic.html HTTP/1.1\r\nContent') is None
+        assert rb.messages['1'].message == 'GET /basic.html HTTP/1.1\r\nContent'
+        assert rb.messages['1'].content_length is None
+
+        assert rb.receive_data('2', 'GET /test.html HTTP/1.1\r\nContent-Length: 4') is None
+        assert rb.messages['2'].message == 'GET /test.html HTTP/1.1\r\nContent-Length: 4'
+        assert rb.messages['2'].content_length == 4
+
+        assert rb.receive_data('1', '-Length: 12\r\n\r\nHello') is None
+        assert rb.messages['1'].message == 'GET /basic.html HTTP/1.1\r\nContent-Length: 12\r\n\r\nHello'
+        assert rb.messages['1'].content_length == 12
+        assert rb.messages['1'].body_bytes == 5
+
+        assert rb.receive_data('2', '\r\n\r\n') is None
+        assert rb.messages['2'].message == 'GET /test.html HTTP/1.1\r\nContent-Length: 4\r\n\r\n'
+        assert rb.messages['2'].content_length == 4
+        assert rb.messages['2'].body_bytes == 0
+
+        assert rb.receive_data('2', 'YOOO') == 'GET /test.html HTTP/1.1\r\nContent-Length: 4\r\n\r\nYOOO'
+
+        assert rb.receive_data('1', ' WORLD') is None
+        assert rb.messages['1'].message == 'GET /basic.html HTTP/1.1\r\nContent-Length: 12\r\n\r\nHello WORLD'
+        assert rb.messages['1'].body_bytes == 11
+        assert rb.messages['1'].content_length == 12
+        assert rb.receive_data('1', '!') == 'GET /basic.html HTTP/1.1\r\nContent-Length: 12\r\n\r\nHello WORLD!'
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
