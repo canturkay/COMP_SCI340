5e92c6eee534cfb7a3c558ac7da2b56d79bb6b42
Adnan Can TURKAY <acturkay@gmail.com>
2020-10-18 21:35:34 +0300

    -

--- Project2/TCPPacket.py
+++ Project2/TCPPacket.py
@@ -1,21 +1,53 @@
 import struct
 
 len_sequence_number = 4
+len_acknowledgement_number = 4
+len_flags = 6
 
 
 class TCPPacket:
     data_bytes = None
-    sequence_number = 0
+    sequence_number = None
+    acknowledgement_number = None
 
-    def unpack(self, packet):
-        packing_format = 'L' + str(len(packet) - len_sequence_number) + 's'
-        self.sequence_number, self.data_bytes = struct.unpack(packing_format, packet)
+    flags = [
+        None, None, None, None, None, None
+    ]
 
-    def pack(self, sequence_number: int, data_bytes: bytes) -> bytes:
-        packing_format = 'L' + str(len(data_bytes)) + 's'
+    def unpack(self, packet) -> None:
+        packing_format = 'II6s' + str(len(packet) - len_sequence_number - len_acknowledgement_number - len_flags) + 's'
+        self.sequence_number, self.acknowledgement_number, flag_bytes, self.data_bytes = struct.unpack(packing_format,
+                                                                                                       packet)
+        self.unpack_flags(flag_bytes)
 
-        self.data_bytes = data_bytes
+    def pack(self, sequence_number: int = 0, acknowledgement_number: int = 0,
+             urgent: bool = False, ack: bool = False, push: bool = False, reset: bool = False,
+             syn: bool = False,
+             fin: bool = False, data_bytes: bytes = b'') -> bytes:
+        packing_format = 'II6s' + str(len(data_bytes)) + 's'
+
         self.sequence_number = sequence_number
+        self.acknowledgement_number = acknowledgement_number
+        self.flags = [urgent, ack, push, reset, syn, fin]
+        self.data_bytes = data_bytes
 
-        return struct.pack(packing_format, self.sequence_number, self.data_bytes)
+        return struct.pack(packing_format, self.sequence_number, self.acknowledgement_number,
+                           self.pack_flags(urgent, ack, push, reset, syn, fin),
+                           self.data_bytes)
 
+    @staticmethod
+    def pack_flags(urgent: bool = False, ack: bool = False, push: bool = False, reset: bool = False,
+                   syn: bool = False,
+                   fin: bool = False) -> bytes:
+        return (b'1' if urgent else b'0') + (b'1' if ack else b'0') + (b'1' if push else b'0') + (
+            b'1' if reset else b'0') + (b'1' if syn else b'0') + (b'1' if fin else b'0')
+
+    def unpack_flags(self, flag_bytes: bytes) -> None:
+        self.flags = [True if flag == b'1' else False for flag in flag_bytes]
+
+    def __str__(self):
+        return str(self.sequence_number) + " | " + str(self.acknowledgement_number) + '\n' + self.data_bytes.decode(
+            'utf-8')
+
+    def __eq__(self, other):
+        return self.sequence_number == other.sequence_number and self.flags == other.flags
--- Project2/streamer.py
+++ Project2/streamer.py
@@ -1,16 +1,23 @@
 # do not import anything else from loss_socket besides LossyUDP
-from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
+import time
+from concurrent.futures import ThreadPoolExecutor
 from socket import INADDR_ANY
 
-import struct
 from TCPPacket import TCPPacket
+from lossy_socket import LossyUDP
 
 
 class Streamer:
     last_sequence_number = 0
 
-    receive_buffer = []
+    receive_buffer = {}
+    ack_buffer = {}
+
+    closed = None
+    executor = None
+    thread = None
+    chunk_size = 1024
 
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
@@ -21,52 +28,95 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
+        self.closed = False
+        self.executor = ThreadPoolExecutor(max_workers=1)
+        self.thread = self.executor.submit(self.recv_async)
+
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        chunk_size = 1024
 
         chunk_index = 0
-        while chunk_index * chunk_size < len(data_bytes):
-            chunk_start_index = chunk_index*chunk_size
-            chunk_end_index = min(len(data_bytes), (chunk_index + 1) * chunk_size)
+        while chunk_index * self.chunk_size < len(data_bytes):
+            chunk_start_index = chunk_index * self.chunk_size
+            chunk_end_index = min(len(data_bytes), (chunk_index + 1) * self.chunk_size)
 
-            packet = TCPPacket()
-            res = packet.pack(self.last_sequence_number, data_bytes[chunk_start_index:chunk_end_index])
+            last_sequence_number = self.last_sequence_number
+            self.ack_buffer[last_sequence_number] = False
+
+            while not self.ack_buffer[last_sequence_number]:
+                print(self.ack_buffer)
+                print([self.receive_buffer[a].flags for a in self.receive_buffer])
+                packet = TCPPacket()
+                res = packet.pack(sequence_number=last_sequence_number,
+                                  data_bytes=data_bytes[chunk_start_index:chunk_end_index])
+
+                self.socket.sendto(res,
+                                   (self.dst_ip, self.dst_port))
+                time.sleep(.01)
+
+            del self.ack_buffer[last_sequence_number]
             self.last_sequence_number += 1
-            packet.unpack(res)
 
-            self.socket.sendto(res,
-                               (self.dst_ip, self.dst_port))
             chunk_index += 1
 
-        # Your code goes here!  The code below should be changed!
-
-        # for now I'm just sending the raw application-level data in one UDP payload
+    def ack(self, acknowledgement_number: int):
+        packet = TCPPacket()
+        res = packet.pack(acknowledgement_number=acknowledgement_number, ack=True)
+        print("WOOO:", packet.flags)
+        self.socket.sendto(res, (self.dst_ip, self.dst_port))
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
 
+        # while len(self.receive_buffer) == 0 or \
+        #         self.receive_buffer[0].sequence_number > self.last_sequence_number:
         while len(self.receive_buffer) == 0 or \
-                self.receive_buffer[0].sequence_number > self.last_sequence_number + 1:
-            data, addr = self.socket.recvfrom()
-            packet = TCPPacket()
-            packet.unpack(data)
+                self.last_sequence_number not in self.receive_buffer:
+            time.sleep(.01)
 
-            self.receive_buffer.append(packet)
-            self.receive_buffer.sort(key=self.sort_func)
-
-        curr = self.receive_buffer.pop(0)
+        # curr = self.receive_buffer.pop(0)
+        curr = self.receive_buffer[self.last_sequence_number]
         self.last_sequence_number += 1
-        print(curr.data_bytes)
-        print(self.last_sequence_number)
 
         return curr.data_bytes
 
-    @staticmethod
-    def sort_func(e: TCPPacket): return e.sequence_number
+    # @staticmethod
+    # def sort_packets(e: TCPPacket):
+    #     return e.sequence_number
+
+    def recv_async(self):
+        while not self.closed:
+            try:
+                data, addr = self.socket.recvfrom()
+                if data is not None and data != b'':
+                    packet = TCPPacket()
+                    packet.unpack(data)
+
+                    if packet.flags[1]:
+                        print("ACK", packet.acknowledgement_number)
+                        self.ack_buffer[packet.acknowledgement_number] = True
+                    else:
+                        print("DATA", packet.sequence_number)
+                        self.ack(packet.sequence_number)
+                        if packet.sequence_number not in self.receive_buffer:
+                            self.receive_buffer[packet.sequence_number] = packet
+                        # self.receive_buffer.sort(key=self.sort_packets)
+                else:
+                    self.closed = True
+            except Exception as e:
+                print("listener died!")
+                print(e)
+        return True
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
+        self.closed = True
+        self.socket.stoprecv()
+
+        while not self.thread.done():
+            self.thread.cancel()
+
+        self.executor.shutdown()
         # your code goes here, especially after you add ACKs and retransmissions.
         pass
