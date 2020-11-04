110e48f375be7cff71cc738887df6e20b6e83f37
Adnan Can TURKAY <acturkay@gmail.com>
2020-11-05 00:15:58 +0300

    Link state implementation

--- Project3/link_state_node.py
+++ Project3/link_state_node.py
@@ -1,7 +1,58 @@
+import datetime
+import heapq
+import json
+
 from simulator.node import Node
 
 
+class Link_State_Edge:
+    cost = None
+    time = None
+
+    def init(self, cost: int, time: datetime.datetime):
+        self.cost = cost
+        self.time = time
+
+    def is_newer_than(self, other: datetime.datetime):
+        return self.time < other
+
+    def from_str(self, message: str):
+        json_value = json.loads(message)
+        self.from_map(json_value)
+
+    def from_map(self, json_value):
+        self.cost = int(json_value["cost"])
+        self.time = datetime.datetime.strptime(json_value["time"], '%Y-%m-%d %H:%M:%S.%f')
+
+    def __str__(self):
+        return json.dumps(
+            {
+                "cost": self.cost,
+                "time": str(self.time)
+            }
+        )
+
+
+class Node_Heap_Object:
+    node_id = None
+    cost = None
+    prev = []
+
+    def __repr__(self):
+        return str(self.node_id) + ": " + str(self.cost) + " - " + str(self.prev)
+
+    def __init__(self, node_id: int, cost: int, prev: list):
+        self.node_id = node_id
+        self.cost = cost
+        self.prev = prev
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
 class Link_State_Node(Node):
+    edges = {}
+
     def __init__(self, id):
         super().__init__(id)
 
@@ -11,13 +62,96 @@ class Link_State_Node(Node):
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
-        # latency = -1 if delete a link
-        pass
+        if latency > -1:
+            if (self.id, neighbor) not in self.edges:
+                link = Link_State_Edge()
+                link.init(cost=latency, time=datetime.datetime.now())
+                self.edges[(self.id, neighbor)] = link
+                self.broadcast_to_neighbors()
+            else:
+                # if not self.edges[(self.id, neighbor)].is_newer_than(datetime.datetime.now()):
+                link = Link_State_Edge()
+                link.init(cost=latency, time=datetime.datetime.now())
+                self.edges[(self.id, neighbor)] = link
+                self.broadcast_to_neighbors()
+        else:
+            if (self.id, neighbor) in self.edges:
+                del self.edges[(self.id, neighbor)]
+                self.broadcast_to_neighbors()
+
+    def broadcast_to_neighbors(self):
+        message = {}
+        for key, val in self.edges.items():
+            message[str(key)] = str(val)
+        self.send_to_neighbors(json.dumps(message))
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
-        pass
+        edges = json.loads(m)
+        changed = False
+
+        for str_key, str_value in edges.items():
+            key = tuple(map(int, str_key[1:-1].split(',')))
+            value = json.loads(str_value)
+            link = Link_State_Edge()
+            link.from_map(value)
+            changed = self.update_edge(source=key[0], destination=key[1], new_edge=link) or changed
+
+        if changed:
+            self.broadcast_to_neighbors()
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        res = self.dijkstra(destination)
+        if res:
+            return res[0]
+        else:
+            return -1
+
+    def dijkstra(self, destination: int) -> list:
+        dist = {}
+        q = [Node_Heap_Object(node_id=self.id, cost=0, prev=[])]
+        dist[self.id] = 0
+        heapq.heapify(q)
+
+        while len(q) > 0:
+            v = heapq.heappop(q)
+            if v.node_id == destination:
+                return v.prev
+
+            for key, val in self.edges.items():
+                neighbor_key = None
+                if key[0] == v.node_id and (key[1] not in dist or dist[key[1]] > val.cost + v.cost):
+                    neighbor_key = key[1]
+                elif key[1] == v.node_id and (key[0] not in dist or dist[key[0]] > val.cost + v.cost):
+                    neighbor_key = key[0]
+
+                if neighbor_key:
+                    heapq.heappush(q,
+                                   Node_Heap_Object(node_id=neighbor_key, cost=val.cost + v.cost,
+                                                    prev=v.prev + [neighbor_key]))
+                    dist[neighbor_key] = val.cost + v.cost
+
+        return None
+
+    def update_edge(self, source: int, destination: int, new_edge: Link_State_Edge) -> bool:
+        src = source
+        dst = destination
+        changed = False
+
+        if destination == self.id:
+            src = destination
+            dst = source
+
+        if (dst, src) in self.edges:
+            self.edges[(src, dst)] = self.edges[(dst, src)]
+            del self.edges[(dst, src)]
+
+        if ((src, dst) in self.edges and new_edge.is_newer_than(self.edges[(src, dst)].time)) or (
+                src, dst) not in self.edges:
+            link = Link_State_Edge()
+            link.init(cost=new_edge.cost, time=new_edge.time)
+            self.edges[(src, dst)] = link
+            changed = True
+
+        return changed
---
+++ Project3/link_state_tests.py
@@ -0,0 +1,59 @@
+import datetime
+
+from link_state_node import Link_State_Node, Link_State_Edge
+
+node = Link_State_Node(id=1)
+
+link1 = Link_State_Edge()
+link1.init(cost=1, time=datetime.datetime.now())
+link2 = Link_State_Edge()
+link2.init(cost=2, time=datetime.datetime.now())
+link3 = Link_State_Edge()
+link3.init(cost=11, time=datetime.datetime.now())
+link4 = Link_State_Edge()
+link4.init(cost=5, time=datetime.datetime.now())
+link5 = Link_State_Edge()
+link5.init(cost=1, time=datetime.datetime.now())
+link6 = Link_State_Edge()
+link6.init(cost=3, time=datetime.datetime.now())
+link7 = Link_State_Edge()
+link7.init(cost=3, time=datetime.datetime.now())
+link8 = Link_State_Edge()
+link8.init(cost=2, time=datetime.datetime.now())
+link9 = Link_State_Edge()
+link9.init(cost=9, time=datetime.datetime.now())
+
+link10 = Link_State_Edge()
+link10.init(cost=2, time=datetime.datetime.now())
+link11 = Link_State_Edge()
+link11.init(cost=8, time=datetime.datetime.now())
+
+
+link12 = Link_State_Edge()
+link12.init(cost=10, time=datetime.datetime.now())
+link13 = Link_State_Edge()
+link13.init(cost=20, time=datetime.datetime.now())
+link14 = Link_State_Edge()
+link14.init(cost=18, time=datetime.datetime.now())
+
+
+node.process_incoming_routing_message(
+    m = "{"
+        "\"(2,1)\": " + str(link1) +
+        ",\"(1,3)\": " + str(link2) +
+        ",\"(6,1)\": " + str(link3) +
+        ",\"(2,4)\": " + str(link4) +
+        ",\"(2,5)\": " + str(link5) +
+        ",\"(3,5)\": " + str(link6) +
+        ",\"(4,6)\": " + str(link7) +
+        ",\"(4,5)\": " + str(link8) +
+        ",\"(5,6)\": " + str(link9) +
+        ",\"(6,7)\": " + str(link10) +
+        ",\"(5,7)\": " + str(link11) +
+        ",\"(7,8)\": " + str(link12) +
+        ",\"(8,5)\": " + str(link13) +
+        ",\"(3,8)\": " + str(link14) +
+        "}"
+)
+
+print(node.dijkstra(destination=8))
