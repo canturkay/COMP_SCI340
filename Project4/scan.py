c69abc7e6b5741f2e1787145a2d5b40de3e24c2d
Adnan Can TURKAY <acturkay@gmail.com>
2020-11-28 20:48:03 +0300

    -

--- Project4/scan.py
+++ Project4/scan.py
@@ -1,10 +1,10 @@
-import time
-import subprocess
 import json
+import subprocess
 import sys
+import time
 
-print(sys.argv)
-def everything():
+
+def main():
     websites = []
 
     with open(sys.argv[1]) as f:
@@ -13,18 +13,18 @@ def everything():
 
     website_scans = {}
     # get scan information for websites and create dictionaries
-    for w in websites:
+    for ws in websites:
         single_website = {}
         single_website["scan_time"] = time.time()
-        ips = get_ip_info(w)
 
-        single_website["ipv4_addresses"] = ips[0]
-        single_website["ipv6_addresses"] = ips[1]
+        single_website["ipv4_addresses"] = get_ipv4_info(ws)
+        single_website["ipv6_addresses"] = get_ipv6_info(ws)
 
-        website_scans[w] = single_website
+        website_scans[ws] = single_website
 
-    #write_to_file(website_scans)
+    # write_to_file(website_scans)
     print(website_scans)
+
 
 def write_to_file(content):
     json_object = json.dumps(content)
@@ -33,18 +33,53 @@ def write_to_file(content):
         json.dump(json_object, f, sort_keys=True, indent=4)
 
 
-def get_ip_info(ws:str):
-    pdr = '208.67.222.222'
-    result = subprocess.check_output(["nslookup", pdr, "8.8.8.8"],
-                                     timeout=2).decode("utf-8")
-    ips = result.split("Name:")[1].split(":", 1)[1].splitlines()
-    ips_rev = [i.strip(" \r\n\t") for i in ips]
-    ipv4_addresses = []
-    ipv6_addresses = []
-    for i in ips_rev:
-        if i != '':
-            if ':' in i and i[0].isnumeric():
-                ipv6_addresses.append(i)
-            if '.' in i and i[0].isnumeric():
-                ipv4_addresses.append(i)
-    return (ipv4_addresses, ipv6_addresses)
+def ns_lookup(ws: str, ipv6: bool = False) -> str:
+    req = []
+    req.append("nslookup")
+    if ipv6:
+        req.append("-type=AAAA")
+
+    req.append(ws)
+    req.append("8.8.8.8")
+
+    return subprocess.check_output(req,
+                                   timeout=2, stderr=subprocess.STDOUT, shell=True).decode("utf-8")
+
+
+def get_ipv4_info(ws: str) -> str:
+    res = ns_lookup(ws)
+    addresses = get_addresses(res, ipv6=False)
+
+    return addresses
+
+
+def get_ipv6_info(ws: str) -> str:
+    res = ns_lookup(ws, ipv6=True)
+    addresses = get_addresses(res, ipv4=False)
+
+    return addresses
+
+
+def get_addresses(lookup_res: str, ipv4: bool = True, ipv6: bool = True) -> list:
+    address_line = lookup_res.split("Name:")[1]
+
+    if ':' not in address_line:
+        return []
+
+    addresses = address_line.split(":", 1)[1].splitlines()
+    ips_rev = [i.strip(" \r\n\t") for i in addresses]
+
+    res = []
+
+    for address in ips_rev:
+        if address != '':
+            if ':' in address and address[0].isnumeric() and ipv6:
+                res.append(address)
+            if '.' in address and address[0].isnumeric() and ipv4:
+                res.append(address)
+
+    return res
+
+
+if __name__ == '__main__':
+    main()
