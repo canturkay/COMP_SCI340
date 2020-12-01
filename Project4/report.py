6b4b34985a8fe3d943eda169c25ac134df7ded37
Adnan Can TURKAY <acturkay@gmail.com>
2020-12-02 01:28:43 +0300

    -

--- Project4/report.py
+++ Project4/report.py
@@ -1,12 +1,14 @@
 import json
 import sys
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from report_packages.report_1 import get_part1_table
 from report_packages.report_2 import get_part2_table
 from report_packages.report_3 import get_part3_table
+from report_packages.report_4 import get_part4_table
+from report_packages.report_5 import get_part5_table
 
 
 def main():
@@ -15,14 +17,20 @@ def main():
 
     df = pd.DataFrame(data).transpose()
 
-    df.rtt_range = df.rtt_range.replace(np.nan, 0)
+    # df.rtt_range = df.rtt_range.replace(np.nan, 0)
 
     df[['rtt_min', 'rtt_max']] = df.rtt_range.to_list()
 
     part_1_table = get_part1_table(data=df)
     part_2_table = get_part2_table(data=df)
     part_3_table = get_part3_table(data=df)
-    write_to_file(content=part_1_table.draw() + '\n' +  part_2_table.draw() + '\n' +  part_3_table.draw(), file_name=sys.argv[2])
+    part_4_table = get_part4_table(data=df)
+    part_5_table = get_part5_table(data=df)
+
+    write_to_file(
+        content=part_1_table.draw() + '\n' + part_2_table.draw() + '\n' + part_3_table.draw() +
+                '\n' + part_4_table.draw() + '\n' + part_5_table.draw(),
+        file_name=sys.argv[2])
 
 
 def write_to_file(content: str, file_name: str):
