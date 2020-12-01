a81fb5585356ab457cf21e89b1368433ea97f587
Adnan Can TURKAY <acturkay@gmail.com>
2020-12-01 20:32:10 +0300

    -

--- Project4/scan.py
+++ Project4/scan.py
@@ -56,7 +56,8 @@ def main():
 
 
 def write_to_file(content):
-    json_object = json.dumps(content)
+    json_object = json.dumps(content, sort_keys=True, indent=4)
+    print(json_object)
     file_name = sys.argv[2]
     with open(file_name, "w") as f:
         json.dump(json_object, f, sort_keys=True, indent=4)
