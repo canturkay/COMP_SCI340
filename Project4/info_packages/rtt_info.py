711c4a0b1491bf19bd9df0aa184f7f5e8ad95bf9
Adnan Can TURKAY <acturkay@gmail.com>
2020-12-02 02:48:54 +0300

    -

--- Project4/info_packages/rtt_info.py
+++ Project4/info_packages/rtt_info.py
@@ -34,13 +34,12 @@ class RTTInfo:
 
     def get_rtt_message(self, ip: str, repeat: int = 0, port: str = '443'):
         try:
-            return subprocess.check_output(
-                ["sh", "-c", "time echo -e '\x1dclose\x0d' | telnet " + ip + " " + port],
-                stderr=subprocess.STDOUT, timeout=10, shell=True).decode("utf-8")
+            req = 'sh -c "time echo -e \'\\x1dclose\\x0d\' | telnet ' + ip + ' ' + port + '"'
+            return subprocess.check_output(req, timeout=2, shell=True,
+                                  stderr=subprocess.STDOUT).decode("utf-8")
         except subprocess.CalledProcessError as e:
             return e.output.decode("utf-8")
         except Exception as ex:
-            print(ex)
             if repeat < 2:
                 return self.get_rtt_message(ip=ip, repeat=repeat + 1)
             else:
